ffd678a6a6f29dcd08bcd5c41db4bb354d2b3b53
Chamoda Pandithage <chamodac@gmail.com>
2016-10-15 19:34:29 +0530

    Create generate.py

---
+++ generate.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+
+import fileinput
+from bs4 import BeautifulSoup
+
+xml = ''
+
+for line in fileinput.input():
+    xml += line
+
+soup = BeautifulSoup(xml, 'xml')
+
+elements = soup.find_all(lambda tag: 'android:id' in tag.attrs)
+
+print '\n// Private variables\n'
+
+for element in elements:
+    print 'private ' + element.name + ' ' + element['android:id'].split("/")[1] + ';'
+
+print '\n// Initialize elemets\n'
+
+for element in elements:
+    print element['android:id'].split("/")[1] + ' = (' + element.name + ')findViewById(R.id.' + element['android:id'].split("/")[1] + ');'
+
+print '\n'
